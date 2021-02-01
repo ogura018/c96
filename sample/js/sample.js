a6fd65a5daddffa9fa0f45079cda9237afa8ecff
ogura018 <72480089+ogura018@users.noreply.github.com>
2021-02-01 18:06:49 +0900

    game

--- sample/js/sample.js
+++ sample/js/sample.js
@@ -3,20 +3,22 @@
  */
 
 
- /** =======================================================================================
- * 1.3 Pixiアプリケーションを生成する
- */ 
+/** =======================================================================================
+* 1.3 Pixiアプリケーションを生成する
+*/
 
 // Pixiアプリケーション生成
 let app = new PIXI.Application({
-    width: 600,                 // スクリーン(ビュー)横幅 
-    height: 600,                // スクリーン(ビュー)縦幅  
-    backgroundColor: 0x1099bb,  // 背景色 16進 0xRRGGBB
+    width: 600,                 // スクリーン(ビュー)横幅
+    height: 600,                // スクリーン(ビュー)縦幅
+    backgroundColor: 0x008000,  // 背景色 16進 0xRRGGBB
     autoDensity: true,
 });
 // HTMLの<main id="app"></main>の中に上で作ったPIXIアプリケーション(app)のビュー(canvas)を突っ込む
 let el = document.getElementById('app');
-el.appendChild(app.view);
+console.log(el)
+console.log(app)
+el.appendChild(app.view); //view canvas
 
 
 
@@ -26,104 +28,125 @@ el.appendChild(app.view);
  * 1.4 スプライトや図形を表示する
  */
 
-// /**
-//  * スプライト(PIXI.Sprite)
-//  */
-// // 画像を読み込み、テクスチャにする
-// let butaTexture = new PIXI.Texture.from('./img/buta.png');
-// // 読み込んだテクスチャから、スプライトを生成する
-// let butaSprite = new PIXI.Sprite(butaTexture);
-// // ぶたの基準点を設定(%) 0.5はそれぞれの中心 位置・回転の基準になる
-// butaSprite.anchor.x = 0.5;
-// butaSprite.anchor.y = 0.5;
-// // ぶたの位置決め
-// butaSprite.x = app.screen.width / 2;        // ビューの幅 / 2 = x中央
-// butaSprite.y = app.screen.height / 2;       // ビューの高さ / 2 = y中央
-// // 表示領域に追加する
-// app.stage.addChild(butaSprite);
+/**
+ * スプライト(PIXI.Sprite)
+ */
+// 画像を読み込み、テクスチャにする
+let butaTexture = new PIXI.Texture.from('./img/buta.png');
+// 読み込んだテクスチャから、スプライトを生成する
+let butaSprite = new PIXI.Sprite(butaTexture);
+// ぶたの基準点を設定(%) 0.5はそれぞれの中心 位置・回転の基準になる
+butaSprite.anchor.x = 0.5;
+butaSprite.anchor.y = 0.5;
+// ぶたの位置決め
+butaSprite.x = app.screen.width / 2;        // ビューの幅 / 2 = x中央
+butaSprite.y = app.screen.height / 2;       // ビューの高さ / 2 = y中央
+// 表示領域に追加する
+app.stage.addChild(butaSprite);
 
 
-// // 別のぶたを作る
-// let butaSprite2 = new PIXI.Sprite(butaTexture); // テクスチャは同じものを使いまわせる
-// // 基準点を設定 set()を使うとx,y同時に設定できる
-// butaSprite2.anchor.set(0.5);
-// // 大きさを変えてみる
-// butaSprite2.scale.x = 1.5;
-// butaSprite2.scale.y = 1.5;
-// // 半透明にしてみる
-// butaSprite2.alpha = 0.9;
-// // 回転してみる
-// butaSprite2.rotation = Math.PI / 3;          // (ラジアンで指定)
-// // butaSprite2.angle = 60;                        // (度数で指定)
-// // 色味を変えてみる
-// butaSprite2.tint = 0xffff00;                // (基準は0xffffff)
+// 別のぶたを作る
+let butaSprite2 = new PIXI.Sprite(butaTexture); // テクスチャは同じものを使いまわせる
+// 基準点を設定 set()を使うとx,y同時に設定できる
+butaSprite2.anchor.set(0.5);
+// 大きさを変えてみる
+butaSprite2.scale.x = 2;
+butaSprite2.scale.y = 2;
+// 半透明にしてみる
+butaSprite2.alpha = 0.9;
+// 回転してみる
+butaSprite2.rotation = Math.PI / 3;          // (ラジアンで指定)
+// butaSprite2.angle = 60;                        // (度数で指定)
+// 色味を変えてみる
+butaSprite2.tint = 0x000000;                // (基準は0xffffff)
 
-// butaSprite2.x = app.screen.width / 2 + 150;
-// butaSprite2.y = app.screen.height / 2;
-// app.stage.addChild(butaSprite2);
+butaSprite2.x = app.screen.width / 2 + 150;
+butaSprite2.y = app.screen.height / 2;
+app.stage.addChild(butaSprite2);
 
+// 画像を読み込み、テクスチャにする
+let suikaTexture = new PIXI.Texture.from('./img/suika.png');
+// 読み込んだテクスチャから、スプライトを生成する
+let suikaSprite = new PIXI.Sprite(suikaTexture);
+// ぶたの基準点を設定(%) 0.5はそれぞれの中心 位置・回転の基準になる
+suikaSprite.anchor.y = 0.5;
+suikaSprite.anchor.x = 0.5;
+// ぶたの位置決め
+suikaSprite.x = app.screen.width / 2;        // ビューの幅 / 2 = x中央
+suikaSprite.y = app.screen.height / 2;       // ビューの高さ / 2 = y中央
+// 表示領域に追加する
+app.stage.addChild(suikaSprite);
 
-
-// /** 
-//  * 図形(PIXI.Graphics) draw~~()はここに書いたもの以外にもあります
-//  */
-
-// // 楕円を作る
-// let ellipse = new PIXI.Graphics()       // メソッドチェーンで描画するので、;(セミコロン)を付けない   
-// .beginFill(0xff0000)                    // endFill()までの描画に対する塗りつぶし色指定
-// .drawEllipse(0,0,30,20)                 // (中心のx座標, 中心のy座標, 幅, 高さ)
-// .endFill();                              // ここまでに描いた図形を塗りつぶす
-
-// // 基準点を設定(px) 図形(PIXI.Graphicsにはpivotはないので注意)
-// ellipse.pivot.x = 15
-// ellipse.pivot.y = 10
-// ellipse.x = 100;
-// ellipse.y = 100;     
-// ellipse.rotation = Math.PI / 6;
-// app.stage.addChild(ellipse);
+//大きさを変える
+suikaSprite.scale.x = 0.25;
+suikaSprite.scale.y = 0.25;
 
 
 
-// // 多角形を作る
-// let polygon = new PIXI.Graphics()
-// .beginFill(0xffffff, 0.8)    // 第二引数で透明度を指定できる
-// .drawPolygon([  // 頂点を配列で渡す [x1,y1,x2,y2,....]
-//                 0, 0,
-//                 25, -20,
-//                 50, 0,
-//                 50, 20,
-//                 25, 40,
-//                 0, 20
-//             ])
-// .endFill();
-// polygon.x = 100;
-// polygon.y = 100;
-// app.stage.addChild(polygon)
+/**
+ * 図形(PIXI.Graphics) draw~~()はここに書いたもの以外にもあります
+ */
+
+// 楕円を作る
+let ellipse = new PIXI.Graphics()       // メソッドチェーンで描画するので、;(セミコロン)を付けない
+    .beginFill(0xff0000)                    // endFill()までの描画に対する塗りつぶし色指定
+    .drawEllipse(0, 0, 30, 20)                 // (中心のx座標, 中心のy座標, 幅, 高さ)
+
+    .endFill()                              // ここまでに描いた図形を塗りつぶす
+
+// 基準点を設定(px) 図形(PIXI.Graphicsにはpivotはないので注意)
+ellipse.pivot.x = 15
+ellipse.pivot.y = 10
+ellipse.x = 100;
+ellipse.y = 100;
+ellipse.rotation = Math.PI / 6;
+app.stage.addChild(ellipse);
 
 
-// // 円を作る(テクスチャを貼る)
-// let circle = new PIXI.Graphics()
-// // 塗りつぶしのかわりにテクスチャを貼る (テクスチャ,色味(リファレンスには背景色って書いてあるからバグかも),透明度,テクスチャのスケール・位置情報)
-// .beginTextureFill(butaTexture, 0x00ffff, 1, new PIXI.Matrix(1,0,0,1,-35,-35))  
-// .lineStyle(2, 0x000000)     // 線のスタイル指定(幅, 色) これ以外に透明度, alignment(線の位置)などが指定可能
-// .drawCircle(0,0,30)   
-// .endFill();
-// circle.x = 200;
-// circle.y = 100;
-// app.stage.addChild(circle);
+
+// 多角形を作る
+let polygon = new PIXI.Graphics()
+    .beginTextureFill(suikaTexture, 0xffffff, 1, new 0.8)    // 第二引数で透明度を指定できる
+    .drawPolygon([  // 頂点を配列で渡す [x1,y1,x2,y2,....]
+        0, 0,
+        25, -20,
+        50, 0,
+        50, 20,
+        25, 40,
+        0, 20
+    ])
+    .endFill();
+polygon.x = 100;
+polygon.y = 100;
+app.stage.addChild(polygon)
 
 
-// // 線を描く
-// let line = new PIXI.Graphics()
-// .lineStyle(1, 0x000000)   // 線のスタイル指定(幅, 色) これ以外に透明度, alignment(線の位置)などが指定可能
-// .moveTo(0,0)              // 開始点に移動
-// .lineTo(50,0)             // (x,y)に向かって直線を引く
-// .lineTo(25,-25)
-// .moveTo(50,0)             // 現在地を移動        
-// .lineTo(25,25);         
-// line.x = 300;
-// line.y = 100;
-// app.stage.addChild(line)
+// 円を作る(テクスチャを貼る)
+
+
+
+let circle = new PIXI.Graphics()
+    // 塗りつぶしのかわりにテクスチャを貼る (テクスチャ,色味(リファレンスには背景色って書いてあるからバグかも),透明度,テクスチャのスケール・位置情報)
+    .beginTextureFill(butaTexture, 0x00ffff, 1, new PIXI.Matrix(1, 0, 0, 1, -35, -35))
+    .lineStyle(2, 0x000000)     // 線のスタイル指定(幅, 色) これ以外に透明度, alignment(線の位置)などが指定可能
+    .drawCircle(0, 0, 30)
+    .endFill();
+circle.x = 200;
+circle.y = 100;
+app.stage.addChild(circle);
+
+
+// 線を描く
+let line = new PIXI.Graphics()
+    .lineStyle(1, 0x000000)   // 線のスタイル指定(幅, 色) これ以外に透明度, alignment(線の位置)などが指定可能
+    .moveTo(0, 0)              // 開始点に移動
+    .lineTo(50, 0)             // (x,y)に向かって直線を引く
+    .lineTo(25, -25)
+    .moveTo(50, 0)             // 現在地を移動
+    .lineTo(25, 25);
+line.x = 300;
+line.y = 100;
+app.stage.addChild(line)
 
 
 
@@ -220,8 +243,8 @@ el.appendChild(app.view);
 // // ぶたの上でマウスがクリック(orタップ)されたときの処理定義
 // function onButaPointerDown() {
 //     butaSprite2.on('pointermove', moveButa);    // ドラッグイベントリスナーを設定
- 
-//     // 分かる人向けTIPS: 
+
+//     // 分かる人向けTIPS:
 //     // ドラッグ処理が重かったり、でかいぶたが他のオブジェクトの下に入ったりするとpointerupを拾えず、
 //     // ドラッグイベントのリスナーが解除されない場合がある。
 //     // こうなるとマウスをクリックした状態でなくても、でかいぶたにマウスが重なるとぶたが追従してくる。
@@ -283,7 +306,7 @@ el.appendChild(app.view);
 //     // ぶたがはまってる円を左右に動かす(適当なのでほっとくとどんどんずれていきます)
 //     amountTime += delta;                    // delta(app.ticker.deltaTime) : 前のフレームから今のフレームまでの経過時間を正規化した値？
 //     // amountTime += app.ticker.deltaMS;    // app.ticker.deltaMS  : 前のフレームから今のフレームまでの経過時間(ms)
-    
+
 //     if (Math.cos(amountTime / 10) > 0) {
 //         // 右に動かす
 //         circle.x += 2;
@@ -366,9 +389,9 @@ el.appendChild(app.view);
 
 
 
- /** =======================================================================================
- * 1.12 Tickerを使わずにアニメーションする(別ライブラリを併用)
- */
+/** =======================================================================================
+* 1.12 Tickerを使わずにアニメーションする(別ライブラリを併用)
+*/
 
 // // ロッカーを4つほど取り出す
 // let l1 = lockers[1];
@@ -376,14 +399,14 @@ el.appendChild(app.view);
 // let l3 = lockers[3];
 // let l4 = lockers[4];
 
-// // TweenMax.to( 対象オブジェクト, 
+// // TweenMax.to( 対象オブジェクト,
 // //              完了までの時間(秒),
-// //              {  
-// //                  pixi: { 
+// //              {
+// //                  pixi: {
 // //                       パラメータ名1: 目標値1, パラメータ名2: 目標値2, ... ,
 // //                  }
 // //                   ease: イージングの形式,
-// //                   repeat: 繰り返し回数 (デフォルトは0、 反復の場合は折り返しもカウントに含む), 
+// //                   repeat: 繰り返し回数 (デフォルトは0、 反復の場合は折り返しもカウントに含む),
 // //                   yoyo: アニメーションを反復するか否か(true/false デフォルトはfalse),
 // //                   delay: アニメーション開始までの遅延時間(秒),
 // //                   onComplete: アニメーション完了時に実行するコールバック
@@ -391,23 +414,23 @@ el.appendChild(app.view);
 // // )
 
 // // 1回だけリピートする
-// TweenMax.to(l1, 0.5, 
-//         {   
-//             pixi: { 
-//                 y: l1.y - 200, 
+// TweenMax.to(l1, 0.5,
+//         {
+//             pixi: {
+//                 y: l1.y - 200,
 //             },
-//             ease: Power0.easeNone, 
+//             ease: Power0.easeNone,
 //             repeat: 1
 //         }
 //     );
 
 // // 反復で1回リピートし、完了時に色を変える
-// TweenMax.to(l2, 0.5, 
-//     {   
-//         pixi: { 
-//             y: l2.y - 200, 
+// TweenMax.to(l2, 0.5,
+//     {
+//         pixi: {
+//             y: l2.y - 200,
 //         },
-//         ease: Power1.easeInOut, 
+//         ease: Power1.easeInOut,
 //         repeat: 2,
 //         yoyo: true,
 //         onComplete: () => { l2.tint = 0xff0000 } // 完了時に色を変える
@@ -415,34 +438,34 @@ el.appendChild(app.view);
 // );
 
 // // 無限リピート
-// TweenMax.to(l3, 0.5, 
-//     {   
-//         pixi: { 
-//             y: l3.y - 200, 
+// TweenMax.to(l3, 0.5,
+//     {
+//         pixi: {
+//             y: l3.y - 200,
 //         },
-//         ease: Power1.easeInOut, 
+//         ease: Power1.easeInOut,
 //         repeat: -1,
 //         yoyo: true
 //     }
-// ); 
+// );
 
 // // 色々詰め込み
 // // 回転
 // TweenMax.to(l4, 1.0, { pixi: { angle: 359}, ease: Power0.easeNone, repeat: -1,});
 // // 色々
-// let l4Tween = TweenMax.to(l4, 1.0, 
-//     {   
-//         pixi: { 
-//             y: l4.y - 200, 
+// let l4Tween = TweenMax.to(l4, 1.0,
+//     {
+//         pixi: {
+//             y: l4.y - 200,
 //             scaleX: l4.scale.x * 1.5,
 //             scaleY: l4.scale.y * 1.5,
 //             tint: 0xff0000,
 //         },
-//         ease: Power1.easeInOut, 
+//         ease: Power1.easeInOut,
 //         repeat: -1,
 //         yoyo: true
 //     }
-// ); 
+// );
 
 
 // // 戻り値のtweenを使ってアニメーションの一時停止や再開ができる
@@ -450,22 +473,22 @@ el.appendChild(app.view);
 
 
 
- /** =======================================================================================
- * 1.13 テキストを表示する(+外部フォントの適用)
- */
+/** =======================================================================================
+* 1.13 テキストを表示する(+外部フォントの適用)
+*/
 
 // PIXI.TextMetrics.BASELINE_SYMBOL += "あ｜";     // 日本語を見切れずに表示するためのおまじない
 
 // // デフォルトのフォントでテキストを表示する
 // // new PIXI.Text(文字列, テキストスタイル(オブジェクト))
-// let text =  new PIXI.Text('あいうアイウABC漢字', 
-//                 { 
+// let text =  new PIXI.Text('あいうアイウABC漢字',
+//                 {
 //                 //   fontFamily: 'Arial',   // フォント
 //                   fontSize: 50,
 //                   fill : 0x000000,       // 文字色
 //                 //   stroke: 0xffffff,      // アウトラインの色
-//                 //   strokeThickness: 3,    // アウトラインの太さ   
-//                 //   align: 'center',       // 文字揃え(複数行の場合に有効)     
+//                 //   strokeThickness: 3,    // アウトラインの太さ
+//                 //   align: 'center',       // 文字揃え(複数行の場合に有効)
 //                 });
 // text.x = 10;
 // text.y = 10;
@@ -478,7 +501,7 @@ el.appendChild(app.view);
 //     // Google Fontsの場合
 //     google:
 //     {
-//         families: ['Noto+Serif+JP']   
+//         families: ['Noto+Serif+JP']
 //     },
 //     // カスタム(自分のサーバーにファイルがあるとか)の場合
 //     custom:
@@ -490,22 +513,22 @@ el.appendChild(app.view);
 //     {
 //         // フォント読み込み成功時
 //         // Google Fontsから読み込んだフォントでテキストを表示する(なぜか漢字が読み込まれない)
-//         let text2 =  new PIXI.Text('あいうアイウABC漢字', 
-//                 { 
+//         let text2 =  new PIXI.Text('あいうアイウABC漢字',
+//                 {
 //                     fontFamily: 'Noto Serif JP',
 //                     fontSize: 50,
-//                     fill : 0x000000,      // 文字色     
+//                     fill : 0x000000,      // 文字色
 //                 });
 //         text2.x = 10;
 //         text2.y = 100;
 //         app.stage.addChild(text2);
 
 //         // カスタムフォントでテキストを表示する
-//         let text3 =  new PIXI.Text('あいうアイウABC漢字', 
-//                 { 
+//         let text3 =  new PIXI.Text('あいうアイウABC漢字',
+//                 {
 //                     fontFamily: '幻ノにじみ明朝',
 //                     fontSize: 50,
-//                     fill : 0x000000,      // 文字色     
+//                     fill : 0x000000,      // 文字色
 //                 });
 //         text3.x = 10;
 //         text3.y = 200;
@@ -522,15 +545,15 @@ el.appendChild(app.view);
 
 
 
- /** =======================================================================================
- * 1.14 点とオブジェクトの衝突判定
- */
+/** =======================================================================================
+* 1.14 点とオブジェクトの衝突判定
+*/
 
 // // 1.9で使ったロッカーを使用する(分かりやすいように基準点を中心に変更)
 // locker.anchor.set(0.5);
 
 // // PIXI.Point(平面上の点オブジェクト)を生成
-// let p = new PIXI.Point();   
+// let p = new PIXI.Point();
 
 // // いくつかのオブジェクトをinteractiveにする(ぶたとでかいぶたは既にinteractive)
 // circle.interactive = true;
@@ -541,7 +564,7 @@ el.appendChild(app.view);
 // l3.interactive = true;
 
 // // 適当に名前をつける
-// butaSprite.namae = "ぶた";       
+// butaSprite.namae = "ぶた";
 // butaSprite2.namae = "でかいぶた";
 // circle.namae = "円"
 // ellipse.namae = "楕円"
@@ -556,7 +579,7 @@ el.appendChild(app.view);
 //         // pにlockerのワールド座標点を代入
 //         // (普通のx,yは親コンテナ基準のローカル座標。これはルートコンテナ(ここではapp.stage))基準の座標)
 //         p.x = locker.transform.worldTransform.tx;
-//         p.y = locker.transform.worldTransform.ty;   
+//         p.y = locker.transform.worldTransform.ty;
 
 //         // 衝突判定(hitTest()はinteractiveがtrueのオブジェクトにのみ有効)
 //         let hitObject = app.renderer.plugins.interaction.hitTest(p, app.stage);  // (判定対象の点, 対象とするオブジェクトの入ったコンテナ)
@@ -574,9 +597,9 @@ el.appendChild(app.view);
 
 
 
- /** =======================================================================================
- * 1.15 オブジェクトの衝突判定領域をカスタマイズする
- */
+/** =======================================================================================
+* 1.15 オブジェクトの衝突判定領域をカスタマイズする
+*/
 
 // // hitArea用の形を作る(これはPIXI.Graphicsと異なり、描画オブジェクトではない)
 // let customHitArea = new PIXI.Polygon(
